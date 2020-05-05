624be96ff9114b8a545bce3465a7b8f2520ecf5c
Gema <gem.anggada@gmail.com>
2020-05-05 17:08:56 +0700

    chore: api to returns images & defined routes for vercel/now

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,35 @@
 import {NowRequest, NowResponse} from "@now/node";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-export default (req: NowRequest, res: NowResponse) => {
-  let clientID = req.body?.clientID;
-  let result = fetch('https://api.unsplash.com/photos/random?count=6', {
-    headers: {
-      Authorization: 'Client-ID 68Flpv0iTyInXg5CJF3pyQFl1LWZE6csk9eYkpungJU'
-    }
-  });
+export default async (req: NowRequest, res: NowResponse) => {
+  let clientID = (req.body || {clientID: null}).clientID;
 
-  console.log('cookies : ', req.cookies);
-  console.log('clientID ?', clientID)
+  /*TODO: cookies
+  let cookies = (req.cookies || {
+      id: null,
+      limit: 10,
+    }) */
+  try {
+    let fetched = await fetch(
+      "https://api.unsplash.com/photos/random?count=6",
+      {
+        headers: {
+          Authorization: `Client-ID ${process.env.UNSPLASH_API_KEY || ''}`
+        }
+      }
+    );
+    let result = await fetched.json();
 
-  res.setHeader('Set-Cookie', 'id=123');
-  res.status(200).json({
-    ...result,
-  });
+    res.setHeader("Set-Cookie", `id=${clientID};Expires=Max-Age`);
+    res.setHeader("Set-Cookie", `limit=10;`);
+
+    res.status(200).json({
+      message: "request success",
+      result
+    });
+  } catch {
+    res.status(500).json({
+      message: "server error"
+    });
+  }
 };
