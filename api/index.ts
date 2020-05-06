c2bce31aa4c0aa736f3abbf3d6d7eeb259759884
Gema <gem.anggada@gmail.com>
2020-05-06 13:08:17 +0700

    update on cookies attempt setting

--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,21 @@ import {NowRequest, NowResponse} from "@now/node";
 import fetch from "node-fetch";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  let clientID = (req.body || {clientID: null}).clientID;
 
-  /*TODO: cookies
-  let cookies = (req.cookies || {
-      id: null,
-      limit: 10,
-    }) */
   try {
+    let {attempt} = req.cookies;
+
+    let numberAttempt = isNaN(Number(attempt)) ? 0 : Number(attempt);
+
+    let newAttempt = attempt ? numberAttempt + 1 : numberAttempt;
+
+    if (newAttempt > 5) {
+      res.status(404).json({
+        message: 'Stop Playing Get A Life!',
+      })
+      return;
+    }
+
     let fetched = await fetch(
       "https://api.unsplash.com/photos/random?count=6",
       {
@@ -18,10 +25,17 @@ export default async (req: NowRequest, res: NowResponse) => {
         }
       }
     );
+
     let result = await fetched.json();
 
-    res.setHeader("Set-Cookie", `id=${clientID};Expires=Max-Age`);
-    res.setHeader("Set-Cookie", `limit=10;`);
+    // TODO: cookies for client id
+    // let clientID = (req.body || {clientID: null}).clientID;
+    // res.setHeader("Set-Cookie", `id=${clientID};Expires=Max-Age`);
+    let expiryDate = new Date(
+      new Date().setDate(new Date().getDate() + 1)
+    ).toString()
+
+    res.setHeader("Set-Cookie", `attempt=${newAttempt};Expires=${expiryDate}`);
 
     res.status(200).json({
       message: "request success",
